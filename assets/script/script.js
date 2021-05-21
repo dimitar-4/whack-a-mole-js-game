d4c6c7ef2000f77df1237cf19377d0a7e8be2c08
dimitar-4 <dcholakov4@gmail.com>
2021-05-21 17:35:13 +0000

    Add game functionality, images and style

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,7 +5,65 @@ const countdownBoard = document.querySelector('.countdown');
 const startButton = document.querySelector('.startButton');
 
 let lastHole;
-let timesUp = false;
+let timeUp = false;
 let timeLimit = 20000;
 let score = 0;
 let countdown;
+
+function pickRandomHole(holes) {
+    const randomHole = Math.floor(Math.random() * holes.length);
+    const hole = holes[randomHole];
+    if (hole === lastHole){
+        return pickRandomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+}
+
+function popOut(){
+    const time = Math.random() * 1300 + 400;
+    const hole = pickRandomHole(holes);
+    hole.classList.add('up');
+    setTimeout(function(){ // !!!!!!!!!!
+        hole.classList.remove('up');
+        if (!timeUp) popOut();
+    }, time);
+}
+
+function startGame(){
+    countdown = timeLimit/1000;
+    scoreBoard.textContent = 0;
+    scoreBoard.style.display = 'block';
+    countdownBoard.textContent = countdown;
+    timeUp = false;
+    score = 0;
+    popOut();
+    setTimeout(function(){
+        timeUp = true;
+    }, timeLimit);
+
+    let startCountdown = setInterval(function(){
+        countdown -= 1;
+        countdownBoard.textContent = countdown;
+        if (countdown < 0) {
+            countdown = 0;
+            clearInterval(startCountdown);
+            countdownBoard.textContent = 'Times UP! Thank you for protecting our planet. This is the way!';
+        }
+    }, 1000);
+}
+
+startButton.addEventListener('click', startGame);
+
+function whack(e){
+    score++;
+    this.style.backgroundImage = 'url("/assets/images/bb-yoda-2.png")';
+    this.style.pointerEvents = 'none';
+    setTimeout(() => {
+        this.style.backgroundImage = 'url("/assets/images/bb-yoda-1.png")';
+        this.style.pointerEvents = 'all';
+    }, 800);
+    scoreBoard.textContent = score;
+}
+
+moles.forEach(mole => mole.addEventListener('click', whack));
